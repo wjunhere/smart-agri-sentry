50309e155e7cb341f1ad1dc836bf739de119f5eb
wjun <wjun099100@gmail.com>
2026-06-03 22:30:35 +0800

    chore(sentry_sensors): add imu_node entry_point and dependencies

--- src/sentry_sensors/setup.py
+++ src/sentry_sensors/setup.py
@@ -10,6 +10,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/imu.launch.py']),
+        ('share/' + package_name + '/config', ['config/imu.yaml', 'config/imu_filter_madgwick.yaml']),
+        ('share/' + package_name + '/udev', ['udev/99-myimu.rules']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -21,6 +24,7 @@ setup(
     entry_points={
         'console_scripts': [
             'uart_bridge_node = sentry_sensors.uart_bridge_node:main',
+            'imu_node = sentry_sensors.imu_node:main',
         ],
     },
 )
