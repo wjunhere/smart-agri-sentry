be465fd92e7d3f3968b86ae36b9f3d0496c60e5d
wjun <wjun099100@gmail.com>
2026-07-13 17:47:19 +0800

    fix: use glob to install all launch files in sentry_bringup

--- src/sentry_bringup/setup.py
+++ src/sentry_bringup/setup.py
@@ -12,9 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', [
-            'launch/sentry_v2.launch.py',
-        ]),
+        ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
         ('share/' + package_name + '/config', [
             '../../config/crop_profiles.yaml',
             '../../config/mission_params.yaml',
